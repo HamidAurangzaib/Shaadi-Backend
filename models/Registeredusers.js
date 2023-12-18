90130cb74d5ee6fabff202ea912e8e04716c77f7
HamidAurangzaib <hamid.aurangzeb@gmail.com>
2023-12-19 03:31:56 +0500

    first commit

---
+++ models/Registeredusers.js
@@ -0,0 +1,20 @@
+import mongoose, { mongo } from "mongoose";
+
+const Registeredusersstruc= mongoose.Schema({
+    
+    fname : String,
+    lname : String,
+    email: String,
+    password: String,
+    age: Number,
+    gender:  String,
+religion:  String,
+cast: String,
+phone: Number,
+city:  String,
+country: String ,
+image: String
+
+
+});
+export const Registeredusersmodel= mongoose.model('Registereduser',Registeredusersstruc);
